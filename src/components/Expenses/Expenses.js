43c3c4121a300dcfc1c858f7115ee2915ae650b9
mujeeb ansari <muzeeb100@yahoo.co.in>
2023-04-07 01:03:23 +0530

    using with the statefull List

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -22,7 +22,14 @@ const Expenses = (props) => {
           selected={filterYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -41,7 +48,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
